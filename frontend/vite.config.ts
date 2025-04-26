2376029047602456a3fb46fe93902a2121270958
Wahyu Amanullah <wahyu.amanullah@gmail.com>
2025-04-26 09:47:40 +0800

    fix: deployment conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/ui",
+  preview: {
+    allowedHosts: ["amanullah.work"],
+  },
   // define: {
   //   "process.env": process.env,
   // },
